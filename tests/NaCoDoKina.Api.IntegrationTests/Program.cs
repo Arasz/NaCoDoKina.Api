aac5676cadde4d08f37068f871923733b492b6fb
Rafał Araszkiewicz <rafal.araszkiewicz@student.put.poznan.pl>
2017-08-28 21:57:54 +0200

    Initialize project structure

---
+++ tests/NaCoDoKina.Api.IntegrationTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NaCoDoKina.Api.IntegrationTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ tests/NaCoDoKina.Api.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NaCoDoKina.Api.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
