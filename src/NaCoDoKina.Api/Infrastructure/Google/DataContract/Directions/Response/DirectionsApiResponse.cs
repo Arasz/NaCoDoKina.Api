441a14502e9af5244c5a95cc0ef568533a28389b
Rafał Araszkiewicz <rafal.araszkiewicz@student.put.poznan.pl>
2017-09-09 18:08:30 +0200

    Add basics direction api tests

--- src/NaCoDoKina.Api/Infrastructure/Google/DataContract/Directions/Response/DirectionsApiResponse.cs
+++ src/NaCoDoKina.Api/Infrastructure/Google/DataContract/Directions/Response/DirectionsApiResponse.cs
@@ -7,13 +7,24 @@ namespace NaCoDoKina.Api.Infrastructure.Google.DataContract.Directions.Response
     [DataContract]
     public class DirectionsApiResponse : GoogleApiResponse
     {
-        [DataMember(Name = "geocoded_waypoints ")]
+        /// <summary>
+        /// Contains an array with details about the geocoding of origin, destination and waypoints. 
+        /// </summary>
+        /// <see cref="https://developers.google.com/maps/documentation/directions/intro#GeocodedWaypoints"/>
+        [DataMember(Name = "geocoded_waypoints")]
         public List<GeocodedWaypoint> GeocodedWaypoints { get; set; }
 
-        [DataMember(Name = "routes ")]
+        /// <summary>
+        /// Contains an array of routes from the origin to the destination. 
+        /// </summary>
+        [DataMember(Name = "routes")]
         public List<Route> Routes { get; set; }
 
-        [DataMember(Name = "available_travel_modes ")]
+        /// <summary>
+        /// Contains an array of available travel modes. This field is returned when a request
+        /// specifies a travel mode and gets no results.
+        /// </summary>
+        [DataMember(Name = "available_travel_modes")]
         public List<string> AvailableTravelModes { get; set; }
     }
 }
---
+++ tests/NaCoDoKina.Api.IntegrationTests/Infrastructure/Google/DirectionsServiceTest.cs
@@ -0,0 +1,62 @@
+﻿using FluentAssertions;
+using NaCoDoKina.Api.Infrastructure.Google.DataContract.Common.Request;
+using NaCoDoKina.Api.Infrastructure.Google.DataContract.Directions;
+using NaCoDoKina.Api.Infrastructure.Google.DataContract.Directions.Request;
+using NaCoDoKina.Api.Infrastructure.Google.Services;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NaCoDoKina.Api.IntegrationTests.Infrastructure.Google
+{
+    public class DirectionsServiceTest : GoogleServiceTestBase<IDirectionsService, DirectionsApiRequest>
+    {
+        protected override IRequestParser<DirectionsApiRequest> RequestParser
+            => new OnlyRequiredDirectionsRequestParser();
+
+        protected override IDirectionsService CreateServiceUnderTest(GoogleServiceDependencies<DirectionsApiRequest> dependencies)
+            => new DirectionsService(dependencies);
+
+        public class GetDirections : DirectionsServiceTest
+        {
+            [Fact]
+            public async Task Should_return_not_empty_directions_for_car()
+            {
+                // arrange
+                var destination = "52.44056,16.919235";
+                var origin = "52.3846579,16.8519869";
+                var request = new DirectionsApiRequest(origin, destination);
+
+                //act
+                var response = await ServiceUnderTest.GetDirections(request);
+
+                response.Status.ShouldBeEquivalentTo("OK");
+                response.ErrorMessage.Should().BeNullOrEmpty();
+                response.Routes.Should().NotBeNullOrEmpty();
+            }
+
+            [Fact]
+            public async Task Should_return_route_with_one_leg_and_duration()
+            {
+                // arrange
+                var destination = "52.44056,16.919235";
+                var origin = "52.3846579,16.8519869";
+                var request = new DirectionsApiRequest(origin, destination);
+
+                //act
+                var response = await ServiceUnderTest.GetDirections(request);
+                var leg = response.Routes
+                    .Select(route => route.Legs.First())
+                    .First();
+
+                response.Status.ShouldBeEquivalentTo("OK");
+                response.ErrorMessage.Should().BeNullOrEmpty();
+                response.Routes.Should().NotBeNullOrEmpty();
+                response.Routes
+                    .SelectMany(route => route.Legs)
+                    .Count().Should().Be(response.Routes.Count);
+                leg.Duration.Value.Should().BeGreaterThan(0);
+            }
+        }
+    }
+}
