ae4e278812b772b131d4659b6acdb113974863b9
Rafał Araszkiewicz <rafal.araszkiewicz@student.put.poznan.pl>
2017-09-15 12:25:21 +0200

    Add cinema repository implementation

---
+++ src/NaCoDoKina.Api/Repositories/CinemaRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using NaCoDoKina.Api.Data;
+using NaCoDoKina.Api.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NaCoDoKina.Api.Repositories
+{
+    public class CinemaRepository : ICinemaRepository
+    {
+        private readonly ApplicationContext _applicationContext;
+        private readonly ILogger<ICinemaRepository> _logger;
+
+        public CinemaRepository(ApplicationContext applicationContext, ILogger<ICinemaRepository> logger)
+        {
+            _applicationContext = applicationContext ?? throw new ArgumentNullException(nameof(applicationContext));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public Task<IEnumerable<Cinema>> GetAllCinemasForMovie(long movieId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Cinema>> GetAllCinemas()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Cinema> AddCinema(Cinema cinema)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
