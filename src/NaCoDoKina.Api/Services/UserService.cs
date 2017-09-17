109f3123bf3c10204f374972c31903ad54a588ed
Rafał Araszkiewicz <rafal.araszkiewicz@student.put.poznan.pl>
2017-09-17 11:35:03 +0200

    Add user service and user servcie test

---
+++ src/NaCoDoKina.Api/Services/UserService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace NaCoDoKina.Api.Services
+{
+    public class UserService : IUserService
+    {
+        public Task<long> GetCurrentUserIdAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ tests/NaCoDoKina.Api.Tests/Services/UserServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NaCoDoKina.Api.Services
+{
+    public class UserServiceTest : ServiceTestBase<IUserService>
+    {
+        public UserServiceTest()
+        {
+            ServiceUnderTest = new UserService();
+        }
+
+        public class GetCurrentUserIdAsync : UserServiceTest
+        {
+            [Fact]
+            public async Task Should_return_current_user_id()
+            {
+                //Arrange
+                var expectedId = 1;
+
+                //Act
+                var userId = await ServiceUnderTest.GetCurrentUserIdAsync();
+
+                //Assert
+                userId.Should().Be(expectedId);
+            }
+        }
+    }
+}
