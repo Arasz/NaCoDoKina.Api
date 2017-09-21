fcc6c33a83f48ad50b5dad541bee1d096fa3bffc
Rafał Araszkiewicz <rafal.araszkiewicz@student.put.poznan.pl>
2017-09-21 19:21:26 +0200

    Add integration http test base

---
+++ tests/NaCoDoKina.Api.IntegrationTests/Api/HttpTestBase.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+
+namespace NaCoDoKina.Api.IntegrationTests.Api
+{
+    /// <inheritdoc/>
+    /// <summary>
+    /// Base class for all integration test using in memory test server implementation 
+    /// </summary>
+    public abstract class HttpTestBase : IDisposable
+    {
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposing)
+                return;
+
+            Server?.Dispose();
+            Client?.Dispose();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected TestServer Server { get; }
+
+        protected HttpClient Client { get; }
+
+        protected virtual Uri BaseAddress => new Uri(@"http://localhost:5000");
+
+        protected virtual string Environment => "Development";
+
+        protected HttpTestBase()
+        {
+            var builder = new WebHostBuilder()
+                .UseEnvironment(Environment)
+                .ConfigureServices(ConfigureServices)
+                .UseStartup<Startup>();
+
+            Server = new TestServer(builder);
+
+            Client = Server.CreateClient();
+            Client.BaseAddress = BaseAddress;
+        }
+
+        protected virtual void ConfigureServices(WebHostBuilderContext webHostBuilderContext, IServiceCollection serviceCollection)
+        {
+        }
+    }
+}
---
+++ tests/NaCoDoKina.Api.Tests/Repository/TestDatabaseContextScope.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NaCoDoKina.Api.Data;
+using System;
+
+namespace NaCoDoKina.Api.Repository
+{
+    public abstract partial class RepositoryTestBase<TRepository>
+    {
+        /// <inheritdoc/>
+        /// <summary>
+        /// Scope in which application context exist 
+        /// </summary>
+        public class TestDatabaseContextScope : IDisposable
+        {
+            public ApplicationContext ApplicationContext { get; }
+
+            public TestDatabaseContextScope(InMemoryDatabaseScope databaseScope)
+            {
+                var options = new DbContextOptionsBuilder<ApplicationContext>()
+                    .EnableSensitiveDataLogging()
+                    .UseSqlite(databaseScope.Connection)
+                    .Options;
+                ApplicationContext = new ApplicationContext(options);
+            }
+
+            public void Dispose()
+            {
+                ApplicationContext.Dispose();
+            }
+        }
+    }
+}
