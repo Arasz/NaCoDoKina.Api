ddb4cbd3209a075273efbf701af9a447ce42659b
Rafał Araszkiewicz <rafal.araszkiewicz@student.put.poznan.pl>
2017-09-23 11:33:52 +0200

    Change project path retrival method

--- tests/NaCoDoKina.Api.IntegrationTests/Api/HttpTestBase.cs
+++ tests/NaCoDoKina.Api.IntegrationTests/Api/HttpTestBase.cs
@@ -2,9 +2,11 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
 using System;
 using System.IO;
 using System.Net.Http;
+using System.Reflection;
 
 namespace NaCoDoKina.Api.IntegrationTests.Api
 {
@@ -39,9 +41,11 @@ namespace NaCoDoKina.Api.IntegrationTests.Api
 
         protected HttpTestBase()
         {
+            var contentRoot = GetProjectPath("src", typeof(Startup).Assembly);
+
             var builder = WebHost.CreateDefaultBuilder()
                 .UseEnvironment(Environment)
-                .UseContentRoot(GetContentRoot())
+                .UseContentRoot(contentRoot)
                 .ConfigureServices(ConfigureServices)
                 .UseStartup<Startup>();
 
@@ -51,16 +55,38 @@ namespace NaCoDoKina.Api.IntegrationTests.Api
             Client.BaseAddress = BaseAddress;
         }
 
-        private string GetContentRoot()
+        /// <summary>
+        /// Gets the full path to the target project path that we wish to test 
+        /// </summary>
+        /// <param name="solutionRelativePath">
+        /// The parent directory of the target project. e.g. src, samples, test, or test/Websites
+        /// </param>
+        /// <param name="startupAssembly"> The target project's assembly. </param>
+        /// <returns> The full path to the target project. </returns>
+        protected static string GetProjectPath(string solutionRelativePath, Assembly startupAssembly)
         {
-            var baseDirectory = new DirectoryInfo(AppContext.BaseDirectory);
+            // Get name of the target project which we want to test
+            var projectName = startupAssembly.GetName().Name;
 
-            while (baseDirectory.Name != "NaCoDoKinaApi")
+            // Get currently executing test project path
+            var applicationBasePath = PlatformServices.Default.Application.ApplicationBasePath;
+
+            // Find the folder which contains the solution file. We then use this information to find
+            // the target project which we want to test.
+            var directoryInfo = new DirectoryInfo(applicationBasePath);
+            do
             {
-                baseDirectory = baseDirectory.Parent;
-            }
+                var solutionFileInfo = new FileInfo(Path.Combine(directoryInfo.FullName, "NaCoDoKinaApi.sln"));
+                if (solutionFileInfo.Exists)
+                {
+                    return Path.GetFullPath(Path.Combine(directoryInfo.FullName, solutionRelativePath, projectName));
+                }
 
-            return Path.Combine(baseDirectory.FullName, "src", "NaCoDoKina.Api");
+                directoryInfo = directoryInfo.Parent;
+            }
+            while (directoryInfo.Parent != null);
+
+            throw new Exception($"Solution root could not be located using application root {applicationBasePath}.");
         }
 
         protected virtual void ConfigureServices(WebHostBuilderContext webHostBuilderContext, IServiceCollection serviceCollection)
