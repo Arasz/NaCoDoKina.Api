615aabbab1de89e3a2f88b141747768b479e88c5
Rafał Araszkiewicz <rafal.araszkiewicz@student.put.poznan.pl>
2017-09-26 01:38:37 +0200

    Add application insights

--- src/NaCoDoKina.Api/Program.cs
+++ src/NaCoDoKina.Api/Program.cs
@@ -13,6 +13,7 @@ namespace NaCoDoKina.Api
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .ConfigureLogging(builder =>
                 {
                     builder.AddSerilog();
