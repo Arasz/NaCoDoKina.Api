c1b9b409730114d6020ac6c3a47f961949d2ac9e
Rafał Araszkiewicz <rafal.araszkiewicz@student.put.poznan.pl>
2017-10-11 13:00:05 +0200

    Add manifest ignore parameter

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,24 +5,22 @@ CONFIGURATION=Release
 RUNTIME=ubuntu-x64
 
 # deploy api
-#ssh arasz@$SERVER_ADDRESS systemctl stop kestrel-nacodokinaapi.service
-
+systemctl -H arasz@$SERVER_ADDRESS stop kestrel-nacodokinaapi.service
 
 dotnet publish src/NaCoDoKina.Api/NaCoDoKina.Api.csproj -c $CONFIGURATION -f $FRAMEWORK_VERSION -r $RUNTIME
 
-rsync -r --delete-after src/NaCoDoKina.Api/bin/$CONFIGURATION/$FRAMEWORK_VERSION/$RUNTIME arasz@$SERVER_ADDRESS:/var/www/nacodokinaapi/
+rsync -r --delete-after src/NaCoDoKina.Api/bin/$CONFIGURATION/$FRAMEWORK_VERSION/$RUNTIME/ arasz@$SERVER_ADDRESS:/var/www/nacodokinaapi/
 
-#ssh arasz@$SERVER_ADDRESS systemctl start kestrel-nacodokinaapi.service
+systemctl -H arasz@$SERVER_ADDRESS start kestrel-nacodokinaapi.service
 
 # deploy hangfire host
 
-#ssh arasz@$SERVER_ADDRESS systemctl stop kestrel-hangfirehost.service
-
+systemctl -H arasz@$SERVER_ADDRESS stop kestrel-hangfirehost.service
 
 dotnet publish src/HangfireHost/HangfireHost.csproj -c $CONFIGURATION -f $FRAMEWORK_VERSION -r $RUNTIME
-rsync -r --delete-after src/HangfireHost/bin/$CONFIGURATION/$FRAMEWORK_VERSION/$RUNTIME  arasz@$SERVER_ADDRESS:/var/www/hangfirehost/
+rsync -r --delete-after src/HangfireHost/bin/$CONFIGURATION/$FRAMEWORK_VERSION/$RUNTIME/  arasz@$SERVER_ADDRESS:/var/www/hangfirehost/
 
-#ssh arasz@$SERVER_ADDRESS systemctl start kestrel-hangfirehost.service
+systemctl -H arasz@$SERVER_ADDRESS start kestrel-hangfirehost.service
 
 
 
