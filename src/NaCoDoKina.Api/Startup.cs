66f35e9ec82875a38ad5bf22c464706292765927
Rafał Araszkiewicz <rafal.araszkiewicz@student.put.poznan.pl>
2017-10-11 14:08:16 +0200

    Modify travis

--- src/NaCoDoKina.Api/Startup.cs
+++ src/NaCoDoKina.Api/Startup.cs
@@ -12,7 +12,6 @@ using Infrastructure.Settings;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -224,10 +223,10 @@ namespace NaCoDoKina.Api
 
             // For reverse proxy authentication
             //https://docs.microsoft.com/en-us/aspnet/core/publishing/linuxproduction?tabs=aspnetcore2x
-            app.UseForwardedHeaders(new ForwardedHeadersOptions
-            {
-                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-            });
+            //app.UseForwardedHeaders(new ForwardedHeadersOptions
+            //{
+            //    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            //});
 
             app.UseAuthentication();
             app.UseMvc();
