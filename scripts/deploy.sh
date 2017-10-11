fd90bf6e9d556b448bb3be890c288390f2749a8f
Rafał Araszkiewicz <rafal.araszkiewicz@student.put.poznan.pl>
2017-10-11 12:07:44 +0200

    Fix deploy file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,24 +5,24 @@ CONFIGURATION=Release
 RUNTIME=ubuntu-x64
 
 # deploy api
-ssh arasz@$SERVER_ADDRESS systemctl stop kestrel-nacodokinaapi.service
+#ssh arasz@$SERVER_ADDRESS systemctl stop kestrel-nacodokinaapi.service
 
 
 dotnet publish src/NaCoDoKina.Api/NaCoDoKina.Api.csproj -c $CONFIGURATION -f $FRAMEWORK_VERSION -r $RUNTIME
 
-rsync -r --delete-after src/NaCoDoKina.Api/bin/$CONFIGURATION/$FRAMEWORK_VERSION/ arasz@$SERVER_ADDRESS:/var/www/nacodokinaapi/
+rsync -r --delete-after src/NaCoDoKina.Api/bin/$CONFIGURATION/$FRAMEWORK_VERSION/$RUNTIME arasz@$SERVER_ADDRESS:/var/www/nacodokinaapi/
 
-ssh arasz@$SERVER_ADDRESS systemctl start kestrel-nacodokinaapi.service
+#ssh arasz@$SERVER_ADDRESS systemctl start kestrel-nacodokinaapi.service
 
 # deploy hangfire host
 
-ssh arasz@$SERVER_ADDRESS systemctl stop kestrel-hangfirehost.service
+#ssh arasz@$SERVER_ADDRESS systemctl stop kestrel-hangfirehost.service
 
 
 dotnet publish src/HangfireHost/HangfireHost.csproj -c $CONFIGURATION -f $FRAMEWORK_VERSION -r $RUNTIME
-rsync -r --delete-after src/HangfireHost/bin/$CONFIGURATION/$FRAMEWORK_VERSION/  arasz@$SERVER_ADDRESS:/var/www/hangfirehost/
+rsync -r --delete-after src/HangfireHost/bin/$CONFIGURATION/$FRAMEWORK_VERSION/$RUNTIME  arasz@$SERVER_ADDRESS:/var/www/hangfirehost/
 
-ssh arasz@$SERVER_ADDRESS systemctl start kestrel-hangfirehost.service
+#ssh arasz@$SERVER_ADDRESS systemctl start kestrel-hangfirehost.service
 
 
 
