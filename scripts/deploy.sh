4abec3e722109452b50369a3fce47a959ec6a2bd
Rafał Araszkiewicz <rafal.araszkiewicz@student.put.poznan.pl>
2017-10-11 11:41:12 +0200

    Add service stoping/starting to deploy step and  self contained publish

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,11 +2,34 @@
 SERVER_ADDRESS=139.59.157.1
 FRAMEWORK_VERSION=netcoreapp2.0
 CONFIGURATION=Release
+RUNTIME=ubuntu-x64
 
-dotnet publish src/NaCoDoKina.Api/NaCoDoKina.Api.csproj -c $CONFIGURATION -f $FRAMEWORK_VERSION
+# deploy api
+ssh arasz@SERVER_ADDRESS
+systemctl stop kestrel-nacodokinaapi.service
+exit
+
+dotnet publish src/NaCoDoKina.Api/NaCoDoKina.Api.csproj -c $CONFIGURATION -f $FRAMEWORK_VERSION -r $RUNTIME
+
 rsync -r --delete-after src/NaCoDoKina.Api/bin/$CONFIGURATION/$FRAMEWORK_VERSION/ arasz@$SERVER_ADDRESS:/var/www/nacodokinaapi/
 
+ssh arasz@SERVER_ADDRESS
+systemctl start kestrel-nacodokinaapi.service
+exit
 
-dotnet publish src/HangfireHost/HangfireHost.csproj -c $CONFIGURATION -f $FRAMEWORK_VERSION
+# deploy hangfire host
+
+ssh arasz@SERVER_ADDRESS
+systemctl stop kestrel-hangfirehost.service
+exit
+
+dotnet publish src/HangfireHost/HangfireHost.csproj -c $CONFIGURATION -f $FRAMEWORK_VERSION -r $RUNTIME
 rsync -r --delete-after src/HangfireHost/bin/$CONFIGURATION/$FRAMEWORK_VERSION/  arasz@$SERVER_ADDRESS:/var/www/hangfirehost/
+
+ssh arasz@SERVER_ADDRESS
+systemctl start kestrel-hangfirehost.service
+
+
+
+
 
