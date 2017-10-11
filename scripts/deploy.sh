0b5c526002f88281bb6bd6f293bf8f13fb00d5fa
Rafał Araszkiewicz <rafal.araszkiewicz@student.put.poznan.pl>
2017-10-11 11:58:32 +0200

    Change ssh call

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,29 +5,25 @@ CONFIGURATION=Release
 RUNTIME=ubuntu-x64
 
 # deploy api
-ssh arasz@$SERVER_ADDRESS
-systemctl stop kestrel-nacodokinaapi.service
-exit
+ssh arasz@$SERVER_ADDRESS systemctl stop kestrel-nacodokinaapi.service
+
 
 dotnet publish src/NaCoDoKina.Api/NaCoDoKina.Api.csproj -c $CONFIGURATION -f $FRAMEWORK_VERSION -r $RUNTIME
 
 rsync -r --delete-after src/NaCoDoKina.Api/bin/$CONFIGURATION/$FRAMEWORK_VERSION/ arasz@$SERVER_ADDRESS:/var/www/nacodokinaapi/
 
-ssh arasz@$SERVER_ADDRESS
-systemctl start kestrel-nacodokinaapi.service
-exit
+ssh arasz@$SERVER_ADDRESS systemctl start kestrel-nacodokinaapi.service
 
 # deploy hangfire host
 
-ssh arasz@$SERVER_ADDRESS
-systemctl stop kestrel-hangfirehost.service
-exit
+ssh arasz@$SERVER_ADDRESS systemctl stop kestrel-hangfirehost.service
+
 
 dotnet publish src/HangfireHost/HangfireHost.csproj -c $CONFIGURATION -f $FRAMEWORK_VERSION -r $RUNTIME
 rsync -r --delete-after src/HangfireHost/bin/$CONFIGURATION/$FRAMEWORK_VERSION/  arasz@$SERVER_ADDRESS:/var/www/hangfirehost/
 
-ssh arasz@$SERVER_ADDRESS
-systemctl start kestrel-hangfirehost.service
+ssh arasz@$SERVER_ADDRESS systemctl start kestrel-hangfirehost.service
+
 
 
 
