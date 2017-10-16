01c34a3db50ff4e2517fe59527dd0cb871033b3a
Rafał Araszkiewicz <rafal.araszkiewicz@student.put.poznan.pl>
2017-10-17 00:32:10 +0200

    Optimize creating showtimes

--- src/ApplicationCore/Entities/Movies/MovieShowtime.cs
+++ src/ApplicationCore/Entities/Movies/MovieShowtime.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using ApplicationCore.Entities.Cinemas;
+﻿using ApplicationCore.Entities.Cinemas;
+using System;
+using System.Collections.Generic;
 
 namespace ApplicationCore.Entities.Movies
 {
@@ -48,5 +49,34 @@ namespace ApplicationCore.Entities.Movies
         {
             return $"{base.ToString()}, {nameof(Language)}: {Language}, {nameof(ShowType)}: {ShowType}, {nameof(ShowTime)}: {ShowTime}, {nameof(Cinema)}: {Cinema}, {nameof(Movie)}: {Movie}";
         }
+
+        private sealed class PreCreateEqualityComparer : IEqualityComparer<MovieShowtime>
+        {
+            public bool Equals(MovieShowtime x, MovieShowtime y)
+            {
+                if (ReferenceEquals(x, y)) return true;
+                if (ReferenceEquals(x, null)) return false;
+                if (ReferenceEquals(y, null)) return false;
+                if (x.GetType() != y.GetType()) return false;
+                return string.Equals(x.ShowType, y.ShowType) && x.ShowTime.Equals(y.ShowTime) && Equals(x.Cinema, y.Cinema) && Equals(x.Movie, y.Movie);
+            }
+
+            public int GetHashCode(MovieShowtime obj)
+            {
+                unchecked
+                {
+                    var hashCode = (obj.ShowType != null ? obj.ShowType.GetHashCode() : 0);
+                    hashCode = (hashCode * 397) ^ obj.ShowTime.GetHashCode();
+                    hashCode = (hashCode * 397) ^ (obj.Cinema != null ? obj.Cinema.GetHashCode() : 0);
+                    hashCode = (hashCode * 397) ^ (obj.Movie != null ? obj.Movie.GetHashCode() : 0);
+                    return hashCode;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Equality comparer used to checking if MovieShowtime is not duplicated 
+        /// </summary>
+        public static IEqualityComparer<MovieShowtime> PreCreateComparer { get; } = new PreCreateEqualityComparer();
     }
 }
---
+++ src/Infrastructure/Migrations/Application/20171016222505_AddIndexOnCinemaAndMovieInMovieShowtime.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Infrastructure.Migrations.Application
+{
+    public partial class AddIndexOnCinemaAndMovieInMovieShowtime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MediaLink_Movies_MovieDetailsId",
+                table: "MediaLink");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ReviewLink_Movies_MovieDetailsId",
+                table: "ReviewLink");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MovieShowtimes_CinemaId",
+                table: "MovieShowtimes");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MovieShowtimes_CinemaId_MovieId",
+                table: "MovieShowtimes",
+                columns: new[] { "CinemaId", "MovieId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MediaLink_Movies_MovieDetailsId",
+                table: "MediaLink",
+                column: "MovieDetailsId",
+                principalTable: "Movies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ReviewLink_Movies_MovieDetailsId",
+                table: "ReviewLink",
+                column: "MovieDetailsId",
+                principalTable: "Movies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MediaLink_Movies_MovieDetailsId",
+                table: "MediaLink");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ReviewLink_Movies_MovieDetailsId",
+                table: "ReviewLink");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MovieShowtimes_CinemaId_MovieId",
+                table: "MovieShowtimes");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MovieShowtimes_CinemaId",
+                table: "MovieShowtimes",
+                column: "CinemaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MediaLink_Movies_MovieDetailsId",
+                table: "MediaLink",
+                column: "MovieDetailsId",
+                principalTable: "Movies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ReviewLink_Movies_MovieDetailsId",
+                table: "ReviewLink",
+                column: "MovieDetailsId",
+                principalTable: "Movies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
